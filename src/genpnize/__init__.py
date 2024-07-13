e7a74f8254f57ecfcccb0f36c472707ecdad382b
ftnext <takuyafjp+develop@gmail.com>
2024-07-13 22:23:30 +0900

    [feat] Pass text (red -> green)

--- src/genpnize/__init__.py
+++ src/genpnize/__init__.py
@@ -1,1 +1,19 @@
+import argparse
+
+from more_itertools import chunked
+
 __version__ = "0.0.1"
+
+
+def main():
+    args = parse_args()
+
+    input_text = args.input.read()
+    for chunk in chunked(input_text.replace("\n", ""), 13):
+        print("".join(chunk))
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=argparse.FileType("r"))
+    return parser.parse_args()
