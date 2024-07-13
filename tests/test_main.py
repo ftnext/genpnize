9630c68a616fae56976a56949d4e9aa2f81a24ab
ftnext <takuyafjp+develop@gmail.com>
2024-07-13 22:52:38 +0900

    [test] cat | genpnize

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import subprocess
 from pathlib import Path
 from unittest.mock import patch
 
@@ -12,3 +13,22 @@ def test_genpnize_from_file_path(capsys):
 
     expected = (resource_dir_path / "file" / "expected.txt").read_text()
     assert capsys.readouterr().out == expected
+
+
+def test_genpnize_from_stdin():
+    resource_dir_path = Path(__file__).parent / "resources"
+    input_path = resource_dir_path / "stdin" / "input.txt"
+
+    cat_process = subprocess.run(
+        ["cat", str(input_path)], stdout=subprocess.PIPE, text=True
+    )
+    genpnize_process = subprocess.run(
+        ["genpnize", "-"],
+        input=cat_process.stdout,
+        text=True,
+        check=True,
+        capture_output=True,
+    )
+
+    expected = (resource_dir_path / "stdin" / "expected.txt").read_text()
+    assert genpnize_process.stdout == expected
