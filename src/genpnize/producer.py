a15ea27a249dce056fbda6f0dc014239b9e35b37
ftnext <takuyafjp+develop@gmail.com>
2024-07-14 19:06:55 +0900

    [refactor] Replace more-itertools

--- src/genpnize/producer.py
+++ src/genpnize/producer.py
@@ -1,7 +1,7 @@
 from collections.abc import Generator
 from typing import Protocol
 
-from more_itertools import chunked
+from genpnize.iter_recipes import grouper
 
 
 class Producer(Protocol):
@@ -10,5 +10,5 @@ class Producer(Protocol):
 
 class GenP:
     def write_lines(self, text: str) -> Generator[str, None, None]:
-        for chunk in chunked(text.replace("\n", ""), 13):
+        for chunk in grouper(text.replace("\n", ""), 13, fillvalue=""):
             yield "".join(chunk)
